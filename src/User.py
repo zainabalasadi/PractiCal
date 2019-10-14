e4befc2e719a1bbbccdb93f68c79e8a9630dd2f3
Zainab Alasadi <zainab.alasadi@hotmail.com>
2019-10-14 14:57:35 +1100

    added contacts and group attributes to user

--- src/User.py
+++ src/User.py
@@ -10,12 +10,18 @@ class User():
         self._email = email
         self._password = password
         self._category = []
+        self._contacts = []
+        self._groups = []
     
     def get_id(self):
         return str(self.id)
 
     def get_firstName(self):
         return self._firstName
+
+    # Validate if provided password matches user password
+    def validate(self, password):
+        return self._password == password
 
     def get_email(self):
         return self._email
@@ -32,6 +38,14 @@ class User():
     def add_categories(self, newCalendar):
         self._calendars.append(newCalendar)
 
-    # Validate if provided password matches user password
-    def validate(self, password):
-        return self._password == password
+    def get_contacts(self):
+        return self._contacts
+
+    def add_contacts(self, contact):
+        self._contacts.append(contact)
+
+    def get_groups(self):
+        return self._groups
+
+    def add_group(self, group):
+        self._groups.append(group)
