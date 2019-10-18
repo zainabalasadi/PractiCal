106defdfc881b23d52620eac94fda203f7508bfe
GreenGroupM <37021220+GreenGroupM@users.noreply.github.com>
2019-10-18 13:08:10 +1100

    Update views.py

--- templates/code/views.py
+++ templates/code/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint
 index_blueprint = Blueprint('index',__name__)
 @index_blueprint.route('/')
-@hindex_blueprint.route('/hello')
+@index_blueprint.route('/hello')
 def index():
  return render_template("index.html")
