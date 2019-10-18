23acca84e792cbf0be788e8f8168344dd5db1662
MTG141 <z5163652@student.unsw.edu.au>
2019-10-18 11:24:48 +1100

    react and weppack set up

---
+++ templates/static/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const resolve = require('path').resolve;
+const config = {
+ devtool: 'eval-source-map',
+ entry: __dirname + '/js/index.jsx',
+ output:{
+      path: resolve('../public'),
+      filename: 'bundle.js',
+      publicPath: resolve('../public')
+},
+ resolve: {
+  extensions: ['.js','.jsx','.css']
+ },
+ module: {
+  rules: [
+  {
+   test: /\.jsx?/,
+   loader: 'babel-loader',
+   exclude: /node_modules/,
+   query:{
+     presets: ['react','es2015']
+   }
+  }]
+ }
+};
+module.exports = config;
