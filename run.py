72e47dc94add17b54acf5bc7ac3c48b007c718a4
GreenGroupM <37021220+GreenGroupM@users.noreply.github.com>
2019-10-18 12:31:30 +1100

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from templates import app
 app = Flask(__name__)
 @app.route('/')
 def index():
  return 'index route'
 if __name__ == '__main__':
+ app.config.from_object('configurations.DevelopmentConfig')
  app.run()
