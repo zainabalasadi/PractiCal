b279e04e8b03559cfc0dcd004f5a3a2af01e14b7
GreenGroupM <37021220+GreenGroupM@users.noreply.github.com>
2019-10-18 12:34:44 +1100

    Update views to support blueprints

--- templates/src/views.py
+++ templates/src/views.py
@@ -1,6 +1,6 @@
-from templates import app
-from flask import render_template
-@app.route('/')
-@app.route('/index')
+from flask import render_template, Blueprint
+hello_blueprint = Blueprint('hello',__name__)
+@hello_blueprint.route('/')
+@hello_blueprint.route('/hello')
 def index():
  return render_template("index.html")
