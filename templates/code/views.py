4109293d662ba1ceb1be799d4dcb92057c3e45b0
GreenGroupM <37021220+GreenGroupM@users.noreply.github.com>
2019-10-18 12:59:24 +1100

    update var names

--- templates/code/views.py
+++ templates/code/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint
-hello_blueprint = Blueprint('hello',__name__)
-@hello_blueprint.route('/')
-@hello_blueprint.route('/hello')
+index_blueprint = Blueprint('index',__name__)
+@index_blueprint.route('/')
+@hindex_blueprint.route('/hello')
 def index():
  return render_template("index.html")
