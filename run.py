fdd76bae4cc7b877a8843873369e9d20d75fdccb
MTG141 <z5163652@student.unsw.edu.au>
2019-10-19 12:07:14 +1100

    react working

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from templates import app
-app = Flask(__name__)
-@app.route('/')
-def index():
- return 'index route'
-if __name__ == '__main__':
- app.config.from_object('configurations.DevelopmentConfig')
- app.run()
+
+app.config.from_object('configurations.DevelopmentConfig')
+app.run(port="5009")
