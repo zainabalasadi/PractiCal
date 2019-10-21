e117397994443e9138c58387bb8fb562175d481d
Zainab Alasadi <zainab.alasadi@hotmail.com>
2019-10-22 09:46:27 +1100

    Removed double import bug

--- templates/__init__.py
+++ templates/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__,
  static_folder = './public',
  template_folder="./static")
-import templates.code.views
 
 from templates.code.views import index_blueprint
 # register the blueprints
