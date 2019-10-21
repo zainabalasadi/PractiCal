42fcabf2b484d2dbad9d612eaf9e9796a061e852
michael <michael.hho98@gmail.com>
2019-10-21 18:03:43 +1100

    blueprint fixed for routes

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from flask import Flask
 from templates import app
 
 app.config.from_object('configurations.DevelopmentConfig')
+app.secret_key = 'mysecret'
 app.run(port="5009")
--- templates/code/views.py
+++ templates/code/views.py
@@ -1,19 +1,10 @@
-from flask import render_template, Blueprint
-index_blueprint = Blueprint('index',__name__)
-@index_blueprint.route('/')
-@index_blueprint.route('/index')
-def index():
- return render_template("index.html")
-
-
-
-
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, render_template, url_for, request, session, redirect, Blueprint
 import bcrypt
 
 app = Flask(__name__)
+index_blueprint = Blueprint('index',__name__)
 
-@app.route('/login', methods=['POST'])
+@index_blueprint.route('/login', methods=['POST'])
 def login():
 	#get database of users
 	#find login name in DB
@@ -27,30 +18,33 @@ def login():
 
 	return 'Invalid username/password'
 
-@app.route('/register', methods=['GET', 'POST'])
+@index_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
 	if request.method == 'POST':
 		#check if username already exists in DB
-		if _________ == request.form['username']:
+		if 'hello' == request.form['username']:
 			return "That username already exists"
 
 		hashpw = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
 		#add this to the DB
 		session['username'] = request.form['username']
-		return redirect(url_for('index'))
+		return redirect(url_for('index.index'))
 
 	return render_template('/register.html')
 
-@app.route('/')
+
+@index_blueprint.route('/')
+@index_blueprint.route('/index')
 def index():
+	# add logout feature
 	if 'username' in session:
 		return 'You are logged in as ' + session['username']
 	return render_template('index.html')
 
-@app.route('/calendar')
+@index_blueprint.route('/calendar')
 def calendar():
+	return 'hi'
 
-
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.secret_key = 'mysecret'
+#     app.run(debug=True)
