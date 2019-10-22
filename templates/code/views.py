9229ad08b0ac10291669db4da0c01c9dabe406de
MTG141 <z5163652@student.unsw.edu.au>
2019-10-22 22:04:28 +1100

    created code for nlp, requires missing PracticalManager functions to be implemented

--- templates/code/views.py
+++ templates/code/views.py
@@ -1,6 +1,42 @@
 from flask import render_template, Blueprint
+import requests
+
 index_blueprint = Blueprint('index',__name__)
 @index_blueprint.route('/')
 @index_blueprint.route('/index')
 def index():
  return render_template("index.html")
+ 
+@blueprint.route("/getIntent/", methods=['POST'])
+def getIntent():
+    textMsg = request.form['message']
+    
+    url = 'https://dialogflow.googleapis.com/v2/{session=projects/practical-proueq/agent/sessions/defaultsessionid}:detectIntent'
+    query = {"queryInput": { "text": textMsg }}
+    
+    result = requests.post(url, data = query)
+    
+    mode = result.form['queryResult']['intent']['displayName']
+    
+    if (mode == "Event scheduling"):
+        date = result.form['queryResult']['parameters']['date']
+        time = result.form['queryResult']['parameters']['time']
+        duration = time + 1
+        
+    #need event creation functions in db manager
+    
+    return
+    
+    
+@blueprint.route("/createEvent/", methods=['POST'])
+def createEvent():
+    user = current_user.get_id()
+    startTime = request.form['start']
+    endTime = request.form['end']
+    name =  request.form['name']
+    desc = request.form['desc']
+    eventId = 1
+    
+    #need event creation functions in db manager
+    
+    return
