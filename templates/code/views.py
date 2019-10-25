b57c1d6291684afb45a88a72424b2460b855f561
MTG141 <z5163652@student.unsw.edu.au>
2019-10-25 12:48:03 +1100

    dialogflow functioning as intended

--- templates/code/views.py
+++ templates/code/views.py
@@ -1,5 +1,13 @@
 from flask import render_template, Blueprint
+import dialogflow_v2
+from google.api_core.exceptions import InvalidArgument
+from protobuf_to_dict import protobuf_to_dict
 import requests
+
+DIALOGFLOW_PROJECT_ID = 'practical-proueq'
+DIALOGFLOW_LANGUAGE_CODE = 'en-US'
+GOOGLE_APPLICATION_CREDENTIALS = 'googlekey.json'
+sessionClient = dialogflow_v2.SessionsClient()
 
 index_blueprint = Blueprint('index',__name__)
 @index_blueprint.route('/')
@@ -10,33 +18,14 @@ def index():
 @blueprint.route("/getIntent/", methods=['POST'])
 def getIntent():
     textMsg = request.form['message']
-    
-    url = 'https://dialogflow.googleapis.com/v2/{session=projects/practical-proueq/agent/sessions/defaultsessionid}:detectIntent'
-    query = {"queryInput": { "text": textMsg }}
-    
-    result = requests.post(url, data = query)
-    
-    mode = result.form['queryResult']['intent']['displayName']
-    
-    if (mode == "Event scheduling"):
-        date = result.form['queryResult']['parameters']['date']
-        time = result.form['queryResult']['parameters']['time']
-        duration = time + 1
-        
-    #need event creation functions in pcal manager
-    
-    return
-    
-    
-@blueprint.route("/createEvent/", methods=['POST'])
-def createEvent():
-    user = current_user.get_id()
-    startTime = request.form['start']
-    endTime = request.form['end']
-    name =  request.form['name']
-    desc = request.form['desc']
-    eventId = 1
-    
-    #need event creation functions in pcal manager
-    
-    return
+
+    SESSION_ID = "100000"
+    session = sessionClient.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+    textInput = dialogflow_v2.types.TextInput(text="Make an event for today at 10pm", language_code=DIALOGFLOW_LANGUAGE_CODE)
+    queryInput = dialogflow_v2.types.QueryInput(text=textInput)
+
+    response = sessionClient.detect_intent(session=session, query_input=queryInput)
+    params = protobuf_to_dict(response.query_result.parameters)
+    if (response.query_result.intent.display_name == "Event scheduling"):
+        return jsonify(params)
