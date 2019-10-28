36bb7d84d34ae435aced3c7fb1c8cdf3308dbbfb
Egene <geney.1997@gmail.com>
2019-10-28 19:00:26 +1100

    added loaduser method for retrieving user from database and loading details into appropriate classes

--- templates/code/PractiCalManager.py
+++ templates/code/PractiCalManager.py
@@ -1,14 +1,21 @@
 # Implementation of PractiCalManager class
 # Completed by Michael Ho
 # Started 15/10/19
+# Edited by Egene Oletu
 
-from calendar import Calendar
-from src.User import User
+from templates.code.Calendar import Calendar
+from templates.code.Comment import Comment
+from templates.code.Event import Event
+#from templates.code.Group import Group
+from templates.code.Notification import Notification
+from templates.code.User import User
+from templates.code.DatabaseManager import DatabaseManager
 
 
 class PractiCalManager():
-    def __init__(self):
+    def __init__(self, database, host, user, password):
         self._users = []
+        self._db = DatabaseManager(database, host, user, password)
 
     def getUsers(self):
         return self._users
@@ -46,3 +53,44 @@ class PractiCalManager():
             if user.get_email() == email:
                 return user
         return None
+
+    def loadUser(self, email, password):
+        user = self._db.getUser(email, password)
+        if user == -1:
+            return None
+
+        events = self._db.getUserEvents(user[0])
+        if events == -1:
+            events = []
+
+        cals = dict()
+        for e in events:
+            e_cal = None
+            calName = e[6]
+            if calName in cals.keys():
+                e_cal = cals[calName]
+            else:
+                #TODO: read calendar colour from settings
+                e_cal = Calendar(calName, 'blue', e[1])
+                cals[calName] = e_cal
+
+            event = Event(eventId=e[0],
+                          user=e[1],
+                          name=e[2],
+                          description=e[3],
+                          startDateTime=e[4],
+                          endDateTime=e[5],
+                          calendar=e_cal)
+            cals[calName].addEvent(event)
+
+        cals = list(cals.values())
+        #TODO: get user notifications, groups, contacts,...
+        u = User(userId=user[0],
+                 firstName=user[1],
+                 lastName=user[2],
+                 email=email,
+                 password=password,
+                 calendars=cals)
+
+        self._users.append(u)
+        return u
