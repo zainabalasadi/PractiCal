7e403509ccd3f88a770aa1728573bef3b979d125
MTG141 <z5163652@student.unsw.edu.au>
2019-10-31 16:41:30 +1100

    views file returns json to http requests

--- templates/code/views.py
+++ templates/code/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, Blueprint
 import dialogflow_v2
 from google.api_core.exceptions import InvalidArgument
-from protobuf_to_dict import protobuf_to_dict
 import requests
 
 DIALOGFLOW_PROJECT_ID = 'practical-proueq'
@@ -15,17 +14,20 @@ index_blueprint = Blueprint('index',__name__)
 def index():
  return render_template("index.html")
  
-@blueprint.route("/getIntent/", methods=['POST'])
+post_blueprint = Blueprint('post',__name__)
+@post_blueprint.route("/getIntent", methods=['POST'])
 def getIntent():
     textMsg = request.form['message']
 
-    SESSION_ID = "100000"
+    SESSION_ID = "Placeholder session" #needs to be replaced with the logged in users id
     session = sessionClient.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 
     textInput = dialogflow_v2.types.TextInput(text="Make an event for today at 10pm", language_code=DIALOGFLOW_LANGUAGE_CODE)
     queryInput = dialogflow_v2.types.QueryInput(text=textInput)
 
     response = sessionClient.detect_intent(session=session, query_input=queryInput)
-    params = protobuf_to_dict(response.query_result.parameters)
     if (response.query_result.intent.display_name == "Event scheduling"):
-        return jsonify(params)
+        return jsonify({"date" : response.query_result.parameters.fields["date"].string_value,
+                        "timeStart" : response.query_result.parameters.fields["timeStart"].string_value,
+                        "timeEnd" : response.query_result.parameters.fields["timeEnd"].string_value
+        })
