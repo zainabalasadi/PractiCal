fcf61ab0f6753f5e5497c9092132036d40d605e2
Derrick Foo <dezza@Derricks-MacBook-Pro.local>
2019-11-02 00:25:00 +1100

    worked on master

--- templates/code/Group.py
+++ templates/code/Group.py
@@ -3,6 +3,7 @@ class Group():
     def __init__(self, name):
         self._name = name
         self._members = []
+        self._events = []
 
     def getMembers(self):
         return self._members
@@ -10,13 +11,31 @@ class Group():
     def addMember(self, member):
         if member not in self.getMembers():
             self._members.append(member)
+            #also add their invitations
 
     def removeMember(self, member):
         if member in self.getMembers():
             self._members.remove(member)
+            #also remove their invittations
 
     def getName(self):
         return self._name
 
     def setName(self, name):
         self._name = name
+
+    def getEvents(self):
+        return self._events
+
+    def addEvent(self, event):
+        if event not in self.getEvents():
+            self._events.append(event)
+            #also invite everyone in group
+
+    def removeEvent(self, event):
+        if event in self.getEvents():
+            self._events.remove(event)
+            #also remove everyone in group
+
+    def updateNotifications(self):
+        return
