87cc671f0760b0ac9aa49b02034fc95ddd3abfad
michael <michael.hho98@gmail.com>
2019-11-13 09:40:55 +1100

    refactor nlp create functionality

--- templates/static/js/components/Calendar.jsx
+++ templates/static/js/components/Calendar.jsx
@@ -112,11 +112,8 @@ class Cal extends Component {
             openSlot: false,
             openEvent: false,
             clickedEvent: {},
-            search: "",
-            nlpText: "",
         };
         this.handleClose = this.handleClose.bind(this);
-        this.handleNlpCreation = this.handleNlpCreation.bind(this);
     };
 
     create_event(event) {
@@ -163,20 +160,6 @@ class Cal extends Component {
             method: 'POST'
 
         }).then((data) => data.json()).then(calendarList => renderComponentsFromList(calendarList));
-    }
-
-
-    handleNlpCreation(e) {
-      console.log(e.target.value)
-      const response = fetch('/nlpCreation', {
-        method: 'POST',
-        body: JSON.stringify({"creationString": e.target.value}),
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        }
-      }).then(response => response.json()).then(data => console.log(data))
-      // TODO Insert code to reflect changes in back end on the front end given the 
-      // response from the fetch request
     }
 
     handleClose() {
@@ -454,7 +437,7 @@ class Cal extends Component {
                   </DialogActions>
                 </Dialog>
             </main>
-<Sidebar />
+            <Sidebar />
             </div>
         );
     }
--- templates/static/js/components/Sidebar.jsx
+++ templates/static/js/components/Sidebar.jsx
@@ -20,7 +20,30 @@ const styles = theme => ({
     },
 });
 
+
+
 class Sidebar extends Component {  
+    constructor() {
+        super()
+        this.state = {
+            nlpText: ""
+        }
+        this.handleNlpCreation = this.handleNlpCreation.bind(this);
+    }
+
+    handleNlpCreation(e) {
+        console.log(e.target.value)
+        const response = fetch('/nlpCreation', {
+          method: 'POST',
+          body: JSON.stringify({"creationString": e.target.value}),
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          }
+        }).then(response => response.json()).then(data => console.log(data))
+        // TODO Insert code to reflect changes in back end on the front end given the 
+        // response from the fetch request
+    }
+
     render() {
         const { classes } = this.props;
         return (
@@ -39,6 +62,15 @@ class Sidebar extends Component {
                   placeholder="Lunch at 2 tomorrow"
                   inputProps={{
                     'aria-label': 'description',
+                  }}
+                  value={ this.state.nlpText }
+                  onChange={e => {
+                    this.setState({ nlpText: e.target.value})
+                  }}
+                  onKeyPress={ e => {
+                    if (e.key === "Enter") {
+                      this.handleNlpCreation(e)
+                    }
                   }}
                 />
                 <h3>My Calendars</h3>
