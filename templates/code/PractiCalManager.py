0b436f91e208a4f040a3fce17e678efb2f577297
Egene <geney.1997@gmail.com>
2019-11-13 16:59:11 +1100

    refactoring

--- templates/code/PractiCalManager.py
+++ templates/code/PractiCalManager.py
@@ -189,7 +189,7 @@ class PractiCalManager():
             inviteeEmails=None):
 
         eventID = self._db.addEvent(userID, title, description, calendar,
-            category, startDateTime, endDateTime, location, jInvitees)
+            category, startDateTime, endDateTime, location)
         if eventID == -1: return None
 
         event = Event(eventID, userID, title, description, startDateTime,
