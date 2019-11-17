7bb64389f5c927f35d54e6df4bfe30adeb419daa
Egene <geney.1997@gmail.com>
2019-11-17 20:52:23 +1100

    merge conflict

--- templates/code/User.py
+++ templates/code/User.py
@@ -66,9 +66,6 @@ class User(UserMixin):
 
     def getCalendarByName(self, name):
         try:
-            # print(name)
-            # for cal in self._calendars:
-                # print(cal)
             cal = self._calendars[name]
             print ("got calendar" + name)
         except:
